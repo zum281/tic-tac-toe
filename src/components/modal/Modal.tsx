c60eb9a5a064d7cec675509d8e7c7a2f664690af
Fulvia <zum281@gmail.com>
2022-06-18 14:24:36 +0200

    feat: added prettier and tie in modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,44 +1,47 @@
-import React, { FunctionComponent as FC, useState } from "react";
-import { useGameContext } from "@context/GameContext";
-import { Grid as GridType } from "@/types/game";
-import { CloseIcon } from "@components/shared/CloseIcon";
-import { Container, Overlay, Close, PlayAgain, Text } from "./Modal.style";
+import React, { FunctionComponent as FC, useState } from 'react'
+import { useGameContext } from '@context/GameContext'
+import { Grid as GridType } from '@/types/game'
+import { CloseIcon } from '@components/shared/CloseIcon'
+import { Container, Overlay, Close, PlayAgain, Text } from './Modal.style'
 
-export const Modal: FC = () => {
-	const { winner, setWinner, setGrid, setCurrentPlayer } = useGameContext();
-	const [isOpen, setIsOpen] = useState(winner ? true : false);
+type Props = {
+	withWinner?: boolean
+}
+
+export const Modal: FC<Props> = ({ withWinner = true }) => {
+	const { winner, setWinner, setGrid, setCurrentPlayer } = useGameContext()
 
 	const resetGame = () => {
-		setWinner(null);
+		setWinner(null)
 		const newGrid: GridType = Array(9)
-			.fill("")
+			.fill('')
 			.map((_, index) => {
-				return { value: "", index };
-			});
-		setGrid([...newGrid]);
-		setCurrentPlayer("X");
-		setIsOpen(false);
-	};
-
-	const closeModal = () => setIsOpen(false);
+				return { value: '', index }
+			})
+		setGrid([...newGrid])
+		setCurrentPlayer('X')
+	}
 
 	return (
 		<>
-			{isOpen && (
-				<>
-					<Overlay onClick={closeModal} />
-					<Container>
-						<Close onClick={closeModal} aria-label='Close'>
-							<CloseIcon />
-						</Close>
-						<Text winner={winner}>
-							<p>The winner is</p>
-							<h2>{winner}</h2>
-						</Text>
-						<PlayAgain onClick={resetGame}>Play again</PlayAgain>
-					</Container>
-				</>
-			)}
+			<Overlay onClick={resetGame} />
+			<Container>
+				<Close onClick={resetGame} aria-label='Close'>
+					<CloseIcon />
+				</Close>
+				{withWinner && (
+					<Text winner={winner}>
+						<p>The winner is</p>
+						<h2>{winner}</h2>
+					</Text>
+				)}
+				{!withWinner && (
+					<Text winner={winner}>
+						<h2>Tie!</h2>
+					</Text>
+				)}
+				<PlayAgain onClick={resetGame}>Play again</PlayAgain>
+			</Container>
 		</>
-	);
-};
+	)
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,20 @@
-import type { NextPage } from "next";
-import { Grid } from "@components/grid";
-import { useGameContext } from "@context/GameContext";
-import { Modal } from "@components/modal";
-import { isGridFull } from "@utils/misc";
+import type { NextPage } from 'next'
+import { Grid } from '@components/grid'
+import { useGameContext } from '@context/GameContext'
+import { Modal } from '@components/modal'
+import { isGridFull } from '@utils/misc'
 
 const Home: NextPage = () => {
-	const { currentPlayer, winner, grid } = useGameContext();
+	const { currentPlayer, winner, grid } = useGameContext()
 
 	return (
 		<div>
 			<h2>Current player: {currentPlayer}</h2>
 			<Grid />
 			{winner && <Modal />}
-			{!winner && isGridFull(grid) && <div>Tie</div>}
+			{!winner && isGridFull(grid) && <Modal withWinner={false} />}
 		</div>
-	);
-};
+	)
+}
 
-export default Home;
+export default Home
